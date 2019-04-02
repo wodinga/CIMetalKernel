7d5411d87ee7dd838c64226792c0bb9af0c8fc2f
David Garcia <david_r_garcia@homedepot.com>
2019-04-02 12:47:58 -0400

    Initial Commit

---
+++ CoreImageTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoreImageTest
+//
+//  Created by David Garcia on 4/2/19.
+//  Copyright © 2019 David Garcia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoreImageTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoreImageTest
+//
+//  Created by David Garcia on 4/2/19.
+//  Copyright © 2019 David Garcia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
